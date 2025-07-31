9ccf7a4efa0e5334528356becc91d59667581e47
Chamika-Samarasinghe <120403416+Chamika-Samarasinghe@users.noreply.github.com>
2025-07-31 17:53:59 +0530

    Completed signup end point

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema= new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     fullName: {
         type: String,
         required: true,
@@ -45,7 +46,24 @@ const userSchema= new mongoose.Schema({
             ref: "User",
         },
     ],
-}, 
-{timestamps:true}
+},
+    { timestamps: true }
 );
 
+const User = mongoose.model("User", userSchema);
+
+//pre hook 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+export default User;
